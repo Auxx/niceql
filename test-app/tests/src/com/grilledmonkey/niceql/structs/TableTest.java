49250acf98374b83bf2bc9a76552ac8142c8e722
Alexander 'Aux' Ivanov <aux@hexmode.org>
2013-04-30 19:31:09 +0300

    Finished test case for Table

--- test-app/tests/src/com/grilledmonkey/niceql/structs/TableTest.java
+++ test-app/tests/src/com/grilledmonkey/niceql/structs/TableTest.java
@@ -1,6 +1,10 @@
 package com.grilledmonkey.niceql.structs;
 
+import java.util.List;
+
 import android.test.AndroidTestCase;
+
+import com.grilledmonkey.niceql.interfaces.SqlColumn;
 
 public class TableTest extends AndroidTestCase {
 	private static final String TABLE_NAME = "table";
@@ -8,5 +12,47 @@ public class TableTest extends AndroidTestCase {
 	public void testConstructor() {
 		Table table = new Table(TABLE_NAME);
 		assertEquals(TABLE_NAME, table.getName());
+		assertEquals(1, table.getColumns().size());
+		assertEquals(0, table.getIndices().size());
+		assertEquals(0, table.getSeeds().size());
+
+		table = new Table(TABLE_NAME, false);
+		assertEquals(TABLE_NAME, table.getName());
+		assertEquals(0, table.getColumns().size());
+	}
+
+	public void testAddColumn() {
+		Table table = new Table(TABLE_NAME, false);
+		table.addColumn(null);
+		table.addColumn(null);
+		assertEquals(2, table.getColumns().size());
+	}
+
+	public void testAddIndex() {
+		Table table = new Table(TABLE_NAME, false);
+		table.addIndex(null);
+		assertEquals(1, table.getIndices().size());
+	}
+
+	public void testAddSeed() {
+		Table table = new Table(TABLE_NAME, false);
+		table.addSeed(null);
+		table.addSeed(null);
+		assertEquals(2, table.getSeeds().size());
+	}
+
+	public void testGetSql() {
+		Table table = new Table(TABLE_NAME);
+		table.addColumn(new Column("title", SqlColumn.TEXT));
+		table.addColumn(new Column("is_visible", SqlColumn.INTEGER));
+		List<String> queries = table.getSql();
+		assertEquals(1, queries.size());
+		assertEquals("CREATE TABLE table(\"_id\" INTEGER  PRIMARY KEY AUTOINCREMENT, \"title\" TEXT, \"is_visible\" INTEGER);", queries.get(0));
+
+		Index index = new Index("index", table);
+		index.addColumn("is_visible");
+		table.addIndex(index);
+		queries = table.getSql();
+		assertEquals(2, queries.size());
 	}
 }
