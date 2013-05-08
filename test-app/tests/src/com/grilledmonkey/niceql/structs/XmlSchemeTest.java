e79598b4e77740cbfcce3df7d0e45e155ba08ed7
Alexander 'Aux' Ivanov <aux@hexmode.org>
2013-05-08 19:48:53 +0300

    #3: XmlScheme test case is complete

--- test-app/tests/src/com/grilledmonkey/niceql/structs/XmlSchemeTest.java
+++ test-app/tests/src/com/grilledmonkey/niceql/structs/XmlSchemeTest.java
@@ -8,6 +8,7 @@ import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import android.content.ContentValues;
 import android.test.AndroidTestCase;
 
 public class XmlSchemeTest extends AndroidTestCase {
@@ -22,8 +23,57 @@ public class XmlSchemeTest extends AndroidTestCase {
 				"</table>" +
 				"</scheme>"));
 		Scheme scheme = XmlScheme.parse(parser);
+
 		List<String> sql = scheme.getSql();
 		assertEquals(1, sql.size());
 		assertEquals("CREATE TABLE articles(\"_id\" INTEGER  PRIMARY KEY AUTOINCREMENT, \"title\" TEXT NOT NULL, \"text\" TEXT);", sql.get(0));
+	}
+
+	public void testParseWithSeeds() throws XmlPullParserException, IOException {
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+		XmlPullParser parser = factory.newPullParser();
+		parser.setInput(new StringReader("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+				"<scheme version=\"1\">" +
+				"<table name=\"articles\" withPrimaryKey=\"true\">" +
+				"<column name=\"title\" type=\"TEXT\" notNull=\"true\" />" +
+				"<column name=\"text\" type=\"TEXT\" />" +
+				"<seed>" +
+				"<column name=\"title\">First article</column>" +
+				"<column name=\"text\">Hello world!</column>" +
+				"</seed>" +
+				"<seed>" +
+				"<column name=\"title\">More stuff to come!</column>" +
+				"<column name=\"text\">Yeah!</column>" +
+				"</seed>" +
+				"</table>" +
+				"</scheme>"));
+		Scheme scheme = XmlScheme.parse(parser);
+
+		List<String> sql = scheme.getSql();
+		assertEquals(1, sql.size());
+
+		List<ContentValues> seeds = scheme.getSeeds();
+		assertEquals(2, seeds.size());
+		assertEquals("First article", seeds.get(0).get("title"));
+		assertEquals("Yeah!", seeds.get(1).get("text"));
+	}
+
+	public void testParseRawSql() throws XmlPullParserException, IOException {
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+		XmlPullParser parser = factory.newPullParser();
+		parser.setInput(new StringReader("<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+				"<scheme version=\"1\">" +
+				"<sql>" +
+				"SELECT 1 FROM articles" +
+				"</sql>" +
+				"<sql>" +
+				"SELECT 2 FROM articles" +
+				"</sql>" +
+				"</scheme>"));
+		Scheme scheme = XmlScheme.parse(parser);
+
+		List<String> sql = scheme.getSql();
+		assertEquals(2, sql.size());
+		assertEquals("SELECT 2 FROM articles", sql.get(1));
 	}
 }
